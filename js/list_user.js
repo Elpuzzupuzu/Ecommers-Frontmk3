9265c3abc256b491a4cf46a0d7d532ef8166f625
Mimit0s <yaelchiruiz97@gmail.com>
2024-07-11 23:45:00 -0600

    implementando el crud para usuarios en el admin candidato 1

---
+++ js/list_user.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('http://localhost:8080/users/all') // Ajusta la URL según sea necesario
+        .then(response => response.json())
+        .then(data => {
+            console.log('Datos obtenidos de la API:', data); // Verifica los datos en la consola
+            const tableBody = document.getElementById('users-table').getElementsByTagName('tbody')[0];
+            // Verifica si data es un array
+            if (Array.isArray(data)) {
+                data.forEach(user => {
+                    const row = document.createElement('tr');
+                    row.innerHTML = `
+                        <td>${user.id}</td>
+                        <td>${user.username}</td>
+                        <td>${user.email}</td>
+                    `;
+                    tableBody.appendChild(row);
+                });
+            } else {
+                console.error('La respuesta no es un array:', data);
+            }
+        })
+        .catch(error => console.error('Error fetching users:', error));
+});
